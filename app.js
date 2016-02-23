f5d463b07b1aa03841887e27fd4745c77e449840
Juscilan Moreto <juscilan.moreto_desenv@hotmail.com>
2016-02-23 16:08:49 -0300

    Acertos

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 
 var http = require('http');
-/*
-//const port = 1225;//process.env.PORT;
-const port = process.env.PORT || 3003;
-console.log(port);
 
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(port);
-*/
 http.createServer(function(req, res){
    res.end('testing anyway..');
 
