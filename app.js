acdbd0a273763e604da937b4af35f4b38aed11bc
Juscilan Moreto <juscilan.moreto_desenv@hotmail.com>
2016-02-23 15:07:11 -0300

    Port

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+//const port = 1225;//process.env.PORT;
 const port = process.env.PORT;
-
+console.log(port);
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
+  
 }).listen(port);
