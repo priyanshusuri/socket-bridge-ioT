029c43ba7a92e3b666168c6a0679bc6228c86de4
Juscilan Moreto <juscilan.moreto_desenv@hotmail.com>
2016-02-23 15:45:43 -0300

    Issue1

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const http = require('http');
 //const port = 1225;//process.env.PORT;
 const port = process.env.PORT || 3003;
 console.log(port);
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-  
 }).listen(port);
+
