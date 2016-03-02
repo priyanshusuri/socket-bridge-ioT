c2ed8db1e1ebba0060fdc2cafb8de8d51620bdbc
Juscilan Moreto <juscilan.moreto_desenv@hotmail.com>
2016-03-02 10:30:58 -0300

    020316

--- serverWeb/index.js
+++ serverWeb/index.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var app = express();
 var port = process.env.PORT ||3000;
 var io = require('socket.io').listen(app.listen(port));
+var http = require('http');
 
 var socketID = null;
  
@@ -15,40 +16,71 @@ app.get("/", function(req, res){
 
 app.get("/:id", function(req, res){
     if(req.params.id == 'reset'){
-        socketID = null;
+        fs.unlinkSync(__dirname + '/configuration.json');
         res.writeHead(200);
-        res.end('Resetado');        
+        res.end('Resetado ');        
     }
     res.writeHead(404);
     res.end('File not found');     
 });   
 
+app.get("/:id/:id", function(req, res){
+    fs.readFile(__dirname + '/configuration.json', 'utf8', function (err,data) {
+    if (err) {
+       res.writeHead(404);
+       res.end('not found');
+    }
+    res.writeHead(200);
+    res.end(data);        
+  });  
+}); 
 
  
 // Iniciando Socket Server
 io.on('connection', function(socket){
-    
+   
     socket.on('ping', function(dados){
-        socketID = {'sessionid':dados};
-        socket.emit('pong', socketID);
-    });     
+        fs.readFile(__dirname + '/configuration.json', 'utf8', function (err,data) {
+            if (err) {
+                return console.log(err);
+            }
+            socket.emit('pong', JSON.parse(data));
+        });
+        
+        http.get({
+            hostname: 'localhost',
+            port: port,
+            path: '/10/10',
+            agent: false  // create a new agent just for this one request
+            }, (res) => {
+                console.log(res);
+            // Do stuff with response
+        })
+    });   
+     
 
     socket.on('setIdServer', function(socket){
-        //Gravar em arquivo a socketIdClient da master na variavel socketID
-        socketID = {'sessionid':socket.socketIdClient};            
+        fs.writeFile(__dirname +'/configuration.json', JSON.stringify(socket), function (err) {
+            if (err) return console.log(err);
+        });            
     });   
 
     socket.on('ligar', function(visitas){
-        socket.broadcast.emit('ligar', socketID);
+        fs.readFile(__dirname + '/configuration.json', 'utf8', function (err,data) {
+            if (err) {
+                return console.log(err);
+            }
+            socket.broadcast.emit('ligar',  JSON.parse(data));
+        });            
+    });   
+ 
+     //Agum navegador entrou no socket?
+    fs.readFile(__dirname + '/configuration.json', 'utf8', function (err,data) {
+        if (!err) {
+            socket.emit('isServer',  JSON.parse(data));
+        }
     });
-    
-    //Agum navegador entrou no socket?
-    //Emite o lbolSocketidClient para avisar que a instancia sera um client
-    if(socketID)
-        socket.emit('isServer', {'sessionid':socketID});
-    });
-            
-console.log("Listening on port " + port);
 
+ });
 
-
+console.log('Running port ' + port)
