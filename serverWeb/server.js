2cd7d4b6630f954be166c83e000cc28b51321428
Juscilan Moreto <juscilan.moreto_desenv@hotmail.com>
2016-03-03 14:23:26 -0300

    030316

---
+++ serverWeb/server.js
@@ -0,0 +1,81 @@
+var port = process.env.PORT ||3000;
+var express = require("express");
+var fs = require('fs');
+var app = express();
+var io = require('socket.io').listen(app.listen(port));
+var http = require('http');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+app.get("/", function(req, res){
+  fs.readFile(__dirname + '/index.html', function(err, data){
+	res.writeHead(200);
+    res.end(data);
+    });
+});   
+
+app.post("/", function(req, res){
+   
+    process.env['SESSIONID'] = JSON.stringify(req.body);
+    res.writeHead(200);
+    res.end('Set ok');     
+
+});
+
+// GET /:id setar a Variavel de ambiente SESSIONID
+app.get("/:id", function(req, res){
+    if(req.params.id == 'reset'){
+        process.env['SESSIONID'] =JSON.stringify({"sessionid":null});
+        res.writeHead(200);
+        res.end('Reset ok');        
+    }
+    else{
+        res.writeHead(404);
+        res.end('File not found');           
+    }
+  
+});   
+
+//
+/*
+app.get("/:id/:id", function(req, res){
+     
+    res.writeHead(200);
+    
+    if(process.env['SESSIONID']){
+        res.end(JSON.stringify(process.env['SESSIONID']) + ' Porta: ' + port );        
+    }else
+        res.end('SESSIONID not found!');
+    
+});
+*/  
+
+ 
+// Iniciando Socket Server
+io.on('connection', function(socket){
+   
+    // Ping-pong utilizado para manter a conexão com o PaaS
+    socket.on('ping', function(dados){
+        socket.emit('pong', JSON.parse(process.env['SESSIONID']));
+    });   
+    
+    // Executa a ação 
+    socket.on('ExecAction', function(data){
+        data.socketid = JSON.parse(process.env['SESSIONID']);
+        socket.broadcast.emit('ExecActionBack',  data);            
+    });   
+
+    //Agum navegador entrou no socket? Envia page load
+    if(process.env['SESSIONID']){
+       socket.emit('isServer',  JSON.parse(process.env['SESSIONID'])); 
+    }else{
+       process.env['SESSIONID'] = JSON.stringify({"sessionid":null});
+       socket.emit('isServer',   process.env['SESSIONID']); 
+    }
+    
+    
+ });
+console.log('Running port ' + port )
