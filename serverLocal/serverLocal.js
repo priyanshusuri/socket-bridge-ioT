143e50cebf7f7379908c1835a1567a476d0073fc
unknown <juscilan@gmail.com>
2016-03-06 19:03:08 -0300

    logica jf

--- serverLocal/serverLocal.js
+++ serverLocal/serverLocal.js
@@ -10,10 +10,10 @@ var app = express();
 var bodyParser = require('body-parser');
 
 
-/*
+
 var five = require("johnny-five");
 var board = new five.Board();
-*/
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -27,26 +27,30 @@ app.use(function(req, res, next) {
 
 app.post('/', function(req, res, next) {
 //Run on arduino
-/*
+
 try {
-
-    var led = new five.Led(10);
-    if(req.body.ledon)
-        led.on();
-    if(req.body.leoff)
-        led.off(); 
-        
-    var rel = new five.Relay(7);               
-    if(req.body.relon)
-        rel.on();
-    if(req.body.reloff)
-        rel.off();               
-
+ 
+    if(req.body.ledon || req.body.ledoff){
+        var led = new five.Led(10);
+        if(req.body.ledon)
+            led.on();
+        if(req.body.ledoff)
+            led.off(); 
+    }
+    
+    if(req.body.relon || req.body.reloff){    
+        var rel = new five.Relay(7);               
+        if(req.body.relon)
+            rel.on();
+        if(req.body.reloff)
+            rel.off();
+    }
+             
     
 } catch (error) {
     res.status(500).json(error);
 }
-*/
+
 
     res.status(200).json({ exec: true });
     console.log(JSON.stringify(req.body));
