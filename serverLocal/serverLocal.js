dafb0be56371ecf0f24d96e32228fb0b75616e83
juscilan <juscilan@gmail.com>
2017-09-03 11:05:43 -0300

    eslint

--- serverLocal/serverLocal.js
+++ serverLocal/serverLocal.js
@@ -1,48 +1,39 @@
-'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const five = require('johnny-five');
 
-/*
-Script NodeJs Adaptado por Juscilan Moreto
-Responsavel por se comunicar com um dispositivo embarcado
-Arduino / Raspberry por exemplo
-2016 © - juscilan.com‎
-*/
-
-const express = require('express'),
-    bodyParser = require('body-parser'),
-    five = require("johnny-five"),
-    app = express(),
-    board = new five.Board();
+const app = express();
 
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true,
 }));
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
 app.post('/', (req, res, next) => {
-    try {
-        if (req.body.ledon || req.body.ledoff) {
-            let led = new five.Led(13);
-            if (req.body.ledon) led.on();
-            if (req.body.ledoff) led.off();
-        }
-    } catch (error) {
-        res.status(500).json(error);
+  try {
+    if (req.body.ledon || req.body.ledoff) {
+      const led = new five.Led(13);
+      if (req.body.ledon) led.on();
+      if (req.body.ledoff) led.off();
     }
-    res.status(200).json({
-        exec: true
-    });
-    console.log(JSON.stringify(req.body));
+  } catch (error) {
+    res.status(500).json(error);
+  }
+  res.status(200).json({
+    exec: true,
+  });
+  console.log(JSON.stringify(req.body));
 });
 
 
 app.listen(3012, () => {
-    console.log('Rodando na porta local: ' + '3012');
+  console.log('Rodando na porta local: 3012');
 });
